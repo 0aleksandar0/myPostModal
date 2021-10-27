f9683ba6859b31bebeb976647895ae012dd6b113
0aleksandar0 <92309291+0aleksandar0@users.noreply.github.com>
2021-10-28 00:26:51 +0200

    Update PostModal.js

--- PostModal.js
+++ PostModal.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import styled from "styled-components";
 import ReactPlayer from "react-player";
 import { connect } from "react-redux";
+import firebase from "firebase";
+import { postArticleAPI } from "../actions";
 
 const PostModal = (props) => {
   const [editorText, setEditorText] = useState("");
@@ -24,6 +26,24 @@ const PostModal = (props) => {
     setShareImage("");
     setVideoLink("");
     setAssetArea(area);
+  };
+
+  const postArticle = (e) => {
+    e.preventDefault();
+    if (e.target !== e.currentTarget) {
+      return;
+    }
+
+    const payload = {
+      image: shareImage,
+      video: videoLink,
+      user: props.user,
+      description: editorText,
+      timestamp: firebase.firestore.Timestamp.now(),
+    };
+
+    props.postArticle(payload);
+    reset(e);
   };
 
   const reset = (e) => {
@@ -114,6 +134,7 @@ const PostModal = (props) => {
               </ShareComment>
               <PostButton
                 disabled={!editorText ? true : false} // if the text is empty disable thee button
+                onClick={(event) => postArticle(event)} // run postArticle function and give it the event details
               >
                 Post
               </PostButton>
@@ -298,6 +319,8 @@ const mapStateToProps = (state) => {
   };
 };
 
-const mapDispatchToProps = (state) => ({});
+const mapDispatchToProps = (dispatch) => ({
+  postArticle: (payload) => dispatch(postArticleAPI(payload)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostModal);
